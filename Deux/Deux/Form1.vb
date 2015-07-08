aa6a7403ad76bbf08c70d316e8f5d2ae04f59bbc
Jarrod Sherrard <jadziank@gmail.com>
2015-07-07 22:30:11 -0400

    Build 1

---
+++ Deux/Deux/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Hello World!"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Print()
+    End Sub
+End Class
--- Hello World/Hello World/Form1.cs
+++ Hello World/Hello World/Form1.cs
@@ -16,5 +16,10 @@ namespace Hello_World
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
